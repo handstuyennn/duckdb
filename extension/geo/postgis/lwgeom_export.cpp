a35e0a1d3bb4c7f312ae238bd7ef78dbf223cab8
Tuyen Nguyen <tuyennn@ihands.com.vn>
2022-11-17 09:38:32 +0700

    update format check

--- extension/geo/postgis/lwgeom_export.cpp
+++ extension/geo/postgis/lwgeom_export.cpp
@@ -8,13 +8,9 @@ lwvarlena_t *LWGEOM_asGeoJson(GSERIALIZED *geom, size_t m_dec_digits) {
 	LWGEOM *lwgeom;
 	int precision = m_dec_digits;
 	int output_bbox = LW_FALSE;
-	int output_long_crs = LW_FALSE;
-	int output_short_crs = LW_FALSE;
-	int output_guess_short_srid = LW_FALSE;
 	const char *srs = NULL;
-	int32_t srid;
-
-	srid = gserialized_get_srid(geom);
+	// int32_t srid;
+	// srid = gserialized_get_srid(geom);
 
 	lwgeom = lwgeom_from_gserialized(geom);
 	auto geojson = lwgeom_to_geojson(lwgeom, srs, precision, output_bbox);
